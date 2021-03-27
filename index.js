cf805f57a5a4825b17678833f061866e88bbc241
LostAndDead <jakemarsh9012@gmail.com>
2021-03-27 15:52:13 +0000

    trying to get it to trigger at :45

--- index.js
+++ index.js
@@ -83,5 +83,5 @@ function getUniqueRoomID(taken){
 }
 
 function msToNextHour() {
-    return 3600000 - new Date().getTime() % 3600000;
+    return (3600000 - new Date().getTime() % 3600000) - 900000;
 }
