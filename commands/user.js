5e23e823f0536290b5839b94ce811b1d74335c55
Daedalline <daedalline@gmail.com>
2022-09-25 10:25:17 -0400

    Fix sort algorithm

--- commands/user.js
+++ commands/user.js
@@ -11,28 +11,28 @@ const { SlashCommandBuilder } = require('@discordjs/builders');
 const fs = require("fs");
 
 module.exports.run = async(interaction, config, maps, client) => {
-	
-	var userID = interaction.options.getUser('user').id
-		
+    
+    var userID = interaction.options.getUser('user').id
+        
     if(interaction.channel.id != config.CoursesLeageChannelID){
         interaction.reply({ephemeral: true, content: "You are not allowed to do that in this channel"})
         return
     }
     await interaction.deferReply();
-	
+    
     let rawdata = await fs.readFileSync('data.json');
     let data = await JSON.parse(rawdata); 
-	
-	var userCourses = {};
-		
-	for (var map in data){
-		var players = data[map];
-		
-		var simpleData = {};
+    
+    var userCourses = {};
+        
+    for (var map in data){
+        var players = data[map];
+        
+        var simpleData = {};
         for(var player in players){
             simpleData[player] = players[player];
         }
-	
+    
         var sortable = [];
         for (var item in simpleData){
             sortable.push([item, simpleData[item]]);
@@ -40,58 +40,60 @@ module.exports.run = async(interaction, config, maps, client) => {
 
         sortable.sort(function(a,b){
             if (a[1][0] == b[1][0]) {
-		    	var dateA = new Date(a[1][1]);
-			    var dateB = new Date(b[1][1]);
-    			if (dateA < dateB){
-				    return -1;
-    			}
-			    else if (dateA > dateB){
-    				return 1;
-			    }
-			    else {
-                    return 0;			
-			    }
-		    }
-            else {	
+                var dateA = new Date(a[1][1]);
+                var dateB = new Date(b[1][1]);
+                if (dateA < dateB){
+                    return -1;
+                }
+                else if (dateA > dateB){
+                    return 1;
+                }
+                else {
+                    return 0;            
+                }
+            }
+            else {    
                 return a[1][0] - b[1][0];
-		    }
+            }
         })
 
+        var index = 0
         var sortedData = {}
         sortable.forEach(function(item){
-            sortedData[item[0]]=item[1][0];
-        })
-		
-		var index = 0
-        for(player in sortedData){
-            if(index >= 10){
+			if(index >= 10){
                 break;
             }
-			userCourses[map] = sortedData[userID];
-            index ++
+            sortedData[item[0]]=item[1][0];
+			index++
+        })
+		
+		console.log(sortedData);
+        
+        for(player in sortedData){
+            userCourses[map] = sortedData[userID];
         }
-	}
-	
-	tbl = "";
-	for (var map in userCourses){
-		if (userCourses[map] != undefined)
-		{
-		    tbl += `${map}: ${userCourses[map]}\n`
-		}
-	}
-	
+    }
+    
+    tbl = "";
+    for (var map in userCourses){
+        if (userCourses[map] != undefined)
+        {
+            tbl += `${map}: ${userCourses[map]}\n`
+        }
+    }
+    
     if(tbl == ""){
         var embed = new Discord.MessageEmbed()
             .setTitle("Database Error")
             .setDescription(`There does not apear to be any scores for **<@${userID}>**`);
         return await interaction.editReply({embeds: [embed]})
     }
-	else {
-	    var embed = new Discord.MessageEmbed()
+    else {
+        var embed = new Discord.MessageEmbed()
         .setTitle(`Leaderboard entries for ` + interaction.options.getUser('user').username)
         .setDescription(tbl);
         return await interaction.editReply({embeds: [embed]})
-	}
+    }
 }
 
 module.exports.info = {
