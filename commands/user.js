0c538cbf92ff52bdeb6ca9f862bc77c3a94bd5ca
Daedalline <33754644+Daedalline@users.noreply.github.com>
2022-09-25 11:08:21 -0400

    List leaderboards by user (#8)
    * Fix user command

---
+++ commands/user.js
@@ -0,0 +1,105 @@
+/*
+ * ====================NOTE====================
+ *    This code was created by Daedalline,
+ *   please don't claim this as your own work
+ *        https://github.com/Daedalline
+ * ============================================
+ */
+
+const Discord = require("discord.js");
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const fs = require("fs");
+
+module.exports.run = async(interaction, config, maps, client) => {
+    
+    var userID = interaction.options.getUser('user').id
+        
+    if(interaction.channel.id != config.CoursesLeageChannelID){
+        interaction.reply({ephemeral: true, content: "You are not allowed to do that in this channel"})
+        return
+    }
+    await interaction.deferReply();
+    
+    let rawdata = await fs.readFileSync('data.json');
+    let data = await JSON.parse(rawdata); 
+    
+    var userCourses = {};
+        
+    for (var map in data){
+        var players = data[map];
+        
+        var simpleData = {};
+        for(var player in players){
+            simpleData[player] = players[player];
+        }
+    
+        var sortable = [];
+        for (var item in simpleData){
+            sortable.push([item, simpleData[item]]);
+        }
+
+        sortable.sort(function(a,b){
+            if (a[1][0] == b[1][0]) {
+                var dateA = new Date(a[1][1]);
+                var dateB = new Date(b[1][1]);
+                if (dateA < dateB){
+                    return -1;
+                }
+                else if (dateA > dateB){
+                    return 1;
+                }
+                else {
+                    return 0;            
+                }
+            }
+            else {    
+                return a[1][0] - b[1][0];
+            }
+        })
+
+        var index = 0
+		for (var i = 0; i<sortable.length; i++) {
+			if (i>=10){
+			    break;
+			}
+			if (sortable[i][0] == userID){
+				userCourses[map] = sortable[i][1][0];
+				break;
+			}
+        }
+    }
+    
+    tbl = "";
+    for (var map in userCourses){
+        if (userCourses[map] != undefined)
+        {
+            tbl += `${map}: ${userCourses[map]}\n`
+        }
+    }
+    
+    if(tbl == ""){
+        var embed = new Discord.MessageEmbed()
+            .setTitle("Database Error")
+            .setDescription(`There does not apear to be any scores for **<@${userID}>**`);
+        return await interaction.editReply({embeds: [embed]})
+    }
+    else {
+        var embed = new Discord.MessageEmbed()
+        .setTitle(`Leaderboard entries for ` + interaction.options.getUser('user').username)
+        .setDescription(tbl);
+        return await interaction.editReply({embeds: [embed]})
+    }
+}
+
+module.exports.info = {
+    "name": "user",
+    "description": "List all Leaderboard scores for a given user",
+    "options": [
+        {
+            "name": "user",
+            "description": "The user to submit for",
+            "type": 6,
+            "required": true
+        }
+    ]
+};
