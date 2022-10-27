daca0659655e0fc41046cf4f07ec2eb16b42bf40
Daedalline <daedalline@gmail.com>
2022-10-27 16:28:42 -0400

    Create top.js

---
+++ commands/top.js
@@ -0,0 +1,41 @@
+/*
+ * ====================NOTE====================
+ *    This code was created by Daedalline,
+ *   please don't claim this as your own work
+ *        https://github.com/Daedalline
+ * ============================================
+ */
+
+const Discord = require("discord.js");
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const fs = require("fs");
+
+module.exports.run = async(interaction, config, maps, client) => {
+    
+    if(interaction.channel.id != config.CoursesLeageChannelID){
+        interaction.reply({ephemeral: true, content: "You are not allowed to do that in this channel"})
+        return
+    }
+    await interaction.deferReply();
+
+    let rawdata = await fs.readFileSync('data.json');
+    let data = await JSON.parse(rawdata); 
+    
+    var sortMapList = [];
+    for (var map in data)
+    {
+        sortMapList.push(map);
+        var sortableScores = [];
+        
+        var players = data[map];
+        for(var player in players){
+            sortableScores.push(players[player][0]);
+        }
+        console.log("Test: " + sortableScores);
+    }
+}
+
+module.exports.info = {
+    "name": "top",
+    "description": "List the top score for each course"
+};
