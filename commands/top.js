8a3ab0c14cde0f1dbb3df684660f58ef8d2aa2be
Daedalline <33754644+Daedalline@users.noreply.github.com>
2022-11-06 11:14:40 -0500

    Minor changes (#20)
    * Update bottom.js
    
    * Update top.js
    
    * Update index.js

--- commands/top.js
+++ commands/top.js
@@ -23,8 +23,10 @@ module.exports.run = async(interaction, config, maps, client) => {
     
     var sortMapList = [];
     var userCourses = {};
-    for (var map in data)
+    
+    for (var i=0;i<maps.Leaderboards.length;i++)
     {
+        var map = maps.Leaderboards[i];
         sortMapList.push(map);
         var sortableScores = [];
         
