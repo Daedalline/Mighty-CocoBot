d482da568fb49c3b6dc3f4501cc21d196740d2de
Daedalline <daedalline@gmail.com>
2022-11-16 14:49:17 -0500

    Update rules.js

--- commands/rules.js
+++ commands/rules.js
@@ -16,40 +16,30 @@ module.exports.run = async(interaction, config, maps, client) => {
     var guild = await client.guilds.cache.find(guild => guild.id == interaction.guild.id)
     var member = await guild.members.cache.find(user => user.id == interaction.member.id)
     
-    var userID = interaction.options.getUser('user').id;
-    await interaction.deferReply();
-    
+   
     if(interaction.options.getSubcommand() == "server_rules"){
-        var embed = new Discord.MessageEmbed()
-        .setTitle("Server Rules")
-        .setDescription(`Hello **<@${userID}>**. Here is a link to the server rules.
+        var content = `Here is a link to the server rules.
         <https://discord.com/channels/752022800562389015/840645351291486218/1012442929496858704>
-        `);
-        return await interaction.editReply({embeds: [embed]});
+        `;
+        client.users.send('id', 'content');
     }
     else if(interaction.options.getSubcommand() == "leaderboard_rules"){
-        var embed = new Discord.MessageEmbed()
-        .setTitle("Leaderboard Rules")
-        .setDescription(`Hello **<@${userID}>**. Here is a link to the leaderboard rules.
+        var content = `Hello **<@${userID}>**. Here is a link to the leaderboard rules.
         <https://discord.com/channels/752022800562389015/966336175843446886/1019302410075783220>
-        `);
-        return await interaction.editReply({embeds: [embed]});        
+        `;
+        client.users.send('id', 'content'); 
     }
     else if(interaction.options.getSubcommand() == "daily_challenge_rules"){
-        var embed = new Discord.MessageEmbed()
-        .setTitle("Daily Challenge Rules")
-        .setDescription(`Hello **<@${userID}>**. Here is a link to the daily challenge rules.
+        var content = `Hello **<@${userID}>**. Here is a link to the daily challenge rules.
         <https://discord.com/channels/752022800562389015/875720382844387328/875794061997539371>
-        `);
-        return await interaction.editReply({embeds: [embed]});        
+        `;
+        client.users.send('id', 'content');       
     }
     else if(interaction.options.getSubcommand() == "tournament_rules"){
-        var embed = new Discord.MessageEmbed()
-        .setTitle("Tournament Rules")
-        .setDescription(`Hello **<@${userID}>**. Here is a link to the tournament rules.
+        var content = `Hello **<@${userID}>**. Here is a link to the tournament rules.
         <https://discord.com/channels/752022800562389015/763542150877020190/1016821272996491265>
-        `);
-        return await interaction.editReply({embeds: [embed]});        
+        `;
+        client.users.send(member, content);     
     } 
 }
 
@@ -60,54 +50,22 @@ module.exports.info = {
         {
             "name": "server_rules",
             "description": "Sends the user a message with a link to the server rules.",
-            "type": 1,
-            "options": [
-                {
-                    "name": "user",
-                    "description": "The user to greet",
-                    "type": 6,
-                    "required": true
-                }
-            ]
+            "type": 1
         },
         {
             "name": "leaderboard_rules",
             "description": "Sends the user a message with a link to the leaderboard rules.",
-            "type": 1,
-            "options": [
-                {
-                    "name": "user",
-                    "description": "The user to greet",
-                    "type": 6,
-                    "required": true
-                }
-            ]
+            "type": 1
         },
         {
             "name": "daily_challenge_rules",
             "description": "Sends the user a message with a link to the daily challenge rules.",
-            "type": 1,
-            "options": [
-                {
-                    "name": "user",
-                    "description": "The user to greet",
-                    "type": 6,
-                    "required": true
-                }
-            ]
+            "type": 1
         },
         {
             "name": "tournament_rules",
             "description": "Sends the user a message with a link to the tournament rules.",
-            "type": 1,
-            "options": [
-                {
-                    "name": "user",
-                    "description": "The user to greet",
-                    "type": 6,
-                    "required": true
-                }
-            ]
+            "type": 1
         }         
     ]
 };
