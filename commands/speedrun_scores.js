7cb09040a7a273fff7c5949e03ecf06cf661d49c
Froman5000 <133929832+Froman5000@users.noreply.github.com>
2023-06-29 14:18:31 -0400

    Froman test branch (#38)
    * Modified the former sppedrun_scores "submit" comand to instead be "submit range" and added a new option called "subtmit time" for mods who already have the Total Time of the run calculated and want to save time.
    
    * Added underscores to command names.
    
    * Added more underscores!
    
    * More underscore stuff... Ugh!

--- commands/speedrun_scores.js
+++ commands/speedrun_scores.js
@@ -24,7 +24,7 @@ module.exports.run = async(interaction, config, maps, client) => {
     let rawdata = await fs.readFileSync('speedrun_data.json');
     let data = await JSON.parse(rawdata); 
 
-    if(interaction.options.getSubcommand() == "submit"){
+    if(interaction.options.getSubcommand() == "submit_range"){
 
         var userID = interaction.options.getUser('user').id;
         var map = interaction.options.getString('map');
@@ -80,6 +80,58 @@ module.exports.run = async(interaction, config, maps, client) => {
         return await interaction.editReply({embeds: [embed]})
     }
     
+    if(interaction.options.getSubcommand() == "submit_time"){
+
+        var userID = interaction.options.getUser('user').id;
+        var map = interaction.options.getString('map');
+        var time = interaction.options.getString('total_time');
+        
+        let timePattern = /\d{1,2}-\d{1,2}-\d{1,2}/;
+        if (!timePattern.test(time)){
+            await interaction.reply({ephemeral: true, content: "Invalid time format. Time must be input in 24 hr format HH-mm-ss."})
+            return;
+        }
+    
+        var totalTime = interaction.options.getString('total_time').split("-");
+
+        await interaction.deferReply()
+        
+        if(!data[map]){
+            data[map] = {}
+        }
+        
+        var totalSeconds = (Number(totalTime[0]) * 3600) + (Number(totalTime[1]) * 60) + (Number(totalTime[2]));
+        
+        if (totalSeconds < 0) 
+        {
+            totalSeconds = 86400 + totalSeconds;
+        }
+        
+        if(data[map][userID] != undefined)
+        {
+            if(data[map][userID][0] <= totalSeconds)
+            {
+                var embed = new Discord.MessageEmbed()
+                .setTitle("No Time Recorded")
+                .setDescription(`This time is greater than the existing time of ` + data[map][userID][0] + " seconds.");
+                return await interaction.editReply({embeds: [embed]})
+            }
+        }
+        
+        data[map][userID] = [totalSeconds, new Date().toJSON()]
+        var writedata = JSON.stringify(data, null, "\t");
+        await fs.writeFileSync('./speedrun_data.json', writedata);
+        
+        var date = new Date(null);
+        date.setSeconds(totalSeconds);
+        var timeString = date.toISOString().slice(11, 19);
+        
+        var embed = new Discord.MessageEmbed()
+        .setTitle("Time Recorded")
+        .setDescription(`Recorded a time of **${timeString}** (${totalSeconds} seconds) for <@${userID}> on **${map}**`);
+        return await interaction.editReply({embeds: [embed]})
+    }
+
     if(interaction.options.getSubcommand() == "remove"){
 
         var userID = interaction.options.getUser('user').id
@@ -133,8 +185,8 @@ module.exports.info = {
     "description": "Allows moderators to manage scores on the boards",
     "options": [
         {
-            "name": "submit",
-            "description": "Submit a users speedrun score to a map.",
+            "name": "submit_range",
+            "description": "Submit a users speedrun score to a map by providing the Start and Current times.",
             "type": 1,
             "options": [
                 {
@@ -160,6 +212,33 @@ module.exports.info = {
                 {
                     "name": "end_time",
                     "description": "Time finish (in 24 hr format HH-mm-ss)",
+                    "type": 3,
+                    "autocomplete": true,
+                    "required": true
+                },
+            ]
+        },
+        {
+            "name": "submit_time",
+            "description": "Submit a users speedrun score to a map by providing the total time.",
+            "type": 1,
+            "options": [
+                {
+                    "name": "user",
+                    "description": "The user to submit for",
+                    "type": 6,
+                    "required": true
+                },
+                {
+                    "name": "map",
+                    "description": "The map they got it on",
+                    "type": 3,
+                    "autocomplete": true,
+                    "required": true
+                },
+                {
+                    "name": "total_time",
+                    "description": "Total Time (in 24 hr format HH-mm-ss)",
                     "type": 3,
                     "autocomplete": true,
                     "required": true
