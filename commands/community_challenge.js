ba56e9eea0e88a53ffa5cf953f105dd1b42336ee
Daedalline <daedalline@gmail.com>
2023-10-28 13:15:00 -0400

    Create community_challenge.js

---
+++ commands/community_challenge.js
@@ -0,0 +1,45 @@
+/*
+ * ====================NOTE====================
+ *    This code was created by Daedalline,
+ *   please don't claim this as your own work
+ *        https://github.com/Daedalline
+ * ============================================
+ */
+
+const Discord = require("discord.js");
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const fs = require("fs");
+
+module.exports.run = async(interaction, config, maps, client) => {
+    var guild = await client.guilds.cache.find(guild => guild.id == interaction.guild.id)
+    var member = await guild.members.cache.find(user => user.id == interaction.member.id)
+
+    if(interaction.channel.id != config.CourseDailyChannelID && !member.permissions.has("MANAGE_MESSAGES")){
+        interaction.reply({ephemeral: true, content: "You are not allowed to do that in this channel"})
+        return
+    }
+    await interaction.deferReply();
+    
+    let rawdata = await fs.readFileSync('daily_challenge_data.json');
+    let challenge_data = await JSON.parse(rawdata);
+
+    if(interaction.options.getSubcommand() == "challenge_list"){
+        for (var i=0;i<challenge_data.length;i++)
+        {
+            console.log(JSON.stringify(challenge_data[i]));
+        }
+    }
+}
+
+module.exports.info = {
+    "name": "community_challenge",
+    "description": "Get details about community challenges",
+    "options": [
+        {
+            "name": "challenge_list",
+            "description": "Lists all community challenge",
+            "type": 1,
+            "options": []
+        }
+    ]
+}
