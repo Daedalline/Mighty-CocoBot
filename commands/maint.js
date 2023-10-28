4d136633aad943b673da831975e5ecb2195be4f6
Daedalline <daedalline@gmail.com>
2023-10-28 12:52:46 -0400

    Update maint.js

--- commands/maint.js
+++ commands/maint.js
@@ -211,7 +211,7 @@ module.exports.run = async(interaction, config, maps, client) => {
         .setDescription(`Removed **${map}**.`);
         return await interaction.editReply({embeds: [embed]})
     }
-    else if(interaction.options.getSubcommand() == "create_challenge"){
+    else if(interaction.options.getSubcommand() == "create_community_challenge"){
         let rawdata = await fs.readFileSync('community_challenge_data.json');
         let challenge_data = await JSON.parse(rawdata);
     
@@ -225,18 +225,14 @@ module.exports.run = async(interaction, config, maps, client) => {
         
         await interaction.deferReply();
         
-        console.log("TEST 1");
-        
         if(challenge_data[name]){
             // Challenge already exists. Output error message.
-            console.log("TEST 2");
             var embed = new Discord.MessageEmbed()
             .setTitle("Database Error")
             .setDescription(`**${name}** already exists.`);
             return await interaction.editReply({embeds: [embed]})
         }
         else {
-            console.log("TEST 3");
             challenge_data[name] = { 
             "Emojii": emojii_parsed,
             "Dates": dates,
