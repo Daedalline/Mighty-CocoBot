c842403d3a4213731f90ffd62d4bbfaf34c3f8de
Daedalline <daedalline@gmail.com>
2023-12-11 16:22:41 -0500

    Update generate_scores.js

--- generate_scores.js
+++ generate_scores.js
@@ -20,7 +20,7 @@ Maps.Leaderboards.forEach(map => {
     for (let i=0; i<numToGenerate; i++)
     {
         let score = Math.floor((Math.random() * -20) + 1);
-        data[map][i] = score;
+        data[map][userID+i] = score;
     }
 });
 
