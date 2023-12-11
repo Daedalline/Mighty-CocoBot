85951ea7eabbe13529287a66a79f6ba89ddba8e8
Daedalline <daedalline@gmail.com>
2023-12-11 17:44:28 -0500

    Update user.js

--- commands/user.js
+++ commands/user.js
@@ -64,9 +64,6 @@ module.exports.run = async(interaction, config, maps, client) => {
 
         var index = 0
         for (var i = 0; i<sortable.length; i++) {
-            if (i>=20){
-                break;
-            }
             if (sortable[i][0] == userID){
                 userCourses[map] = sortable[i][1][0];
                 break;
