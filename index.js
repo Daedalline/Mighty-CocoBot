18be07f8dc2d88a6d490a26d796f1ef06a686afa
Daedalline <daedalline@gmail.com>
2024-04-25 09:35:41 -0400

    Update index.js

--- index.js
+++ index.js
@@ -354,7 +354,7 @@ async function printLanguageGameMessage() {
     let currentDateSubstring = currentDateString.substr(0, currentDateString.length - 3);
 
     let embed = new MessageEmbed()
-    .setTitle(":flag_fr: :flag_de: :flag_it: :flag_es: Non-English games starting soon! :flag_es: :flag_it: :flag_de: :flag_fr: ")
+    .setTitle(":flag_fr: :flag_de: :flag_it: :flag_es: Non-English games starting soon! :flag_es: :flag_it: :flag_de: :flag_fr:")
     .setDescription(`
     The next scheduled non-English games will start in **15 minutes** (at <t:${currentDateSubstring}:t>).
     
