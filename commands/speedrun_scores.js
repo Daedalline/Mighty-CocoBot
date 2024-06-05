5aa5127df845654d9420b24dbb6ca3c5e564ae99
Daedalline <daedalline@gmail.com>
2024-06-05 14:37:43 -0400

    Update speedrun_scores.js

--- commands/speedrun_scores.js
+++ commands/speedrun_scores.js
@@ -24,62 +24,6 @@ module.exports.run = async(interaction, config, maps, client) => {
     let rawdata = await fs.readFileSync('speedrun_data.json');
     let data = await JSON.parse(rawdata); 
 
-    if(interaction.options.getSubcommand() == "submit_range"){
-
-        var userID = interaction.options.getUser('user').id;
-        var map = interaction.options.getString('map');
-        var startTimeString = interaction.options.getString('start_time');
-        var endTimeString = interaction.options.getString('end_time');
-        
-        let timePattern = /\d{1,2}-\d{1,2}-\d{1,2}/;
-        if (!timePattern.test(startTimeString) || !timePattern.test(endTimeString)){
-            await interaction.reply({ephemeral: true, content: "Invalid time format. Time must be input in 24 hr format HH-mm-ss."})
-            return;
-        }
-    
-        var startTime = interaction.options.getString('start_time').split("-");
-        var endTime = interaction.options.getString('end_time').split("-");
-
-        await interaction.deferReply()
-        
-        if(!data[map]){
-            data[map] = {}
-        }
-        
-        var startTimeInSeconds = (Number(startTime[0]) * 3600) + (Number(startTime[1]) * 60) + (Number(startTime[2]));
-        var endTimeInSeconds = (Number(endTime[0]) * 3600) + (Number(endTime[1]) * 60) + (Number(endTime[2]));
-        var totalSeconds = endTimeInSeconds - startTimeInSeconds;
-        
-        if (totalSeconds < 0) 
-        {
-            totalSeconds = 86400 + totalSeconds;
-        }
-        
-        if(data[map][userID] != undefined)
-        {
-            if(data[map][userID][0] <= totalSeconds)
-            {
-                var embed = new Discord.MessageEmbed()
-                .setTitle("No Time Recorded")
-                .setDescription(`This time is greater than the existing time of ` + data[map][userID][0] + " seconds.");
-                return await interaction.editReply({embeds: [embed]})
-            }
-        }
-        
-        data[map][userID] = [totalSeconds, new Date().toJSON()]
-        var writedata = JSON.stringify(data, null, "\t");
-        await fs.writeFileSync('./speedrun_data.json', writedata);
-        
-        var date = new Date(null);
-        date.setSeconds(totalSeconds);
-        var timeString = date.toISOString().slice(11, 19);
-        
-        var embed = new Discord.MessageEmbed()
-        .setTitle("Time Recorded")
-        .setDescription(`Recorded a time of **${timeString}** (${totalSeconds} seconds) for <@${userID}> on **${map}**`);
-        return await interaction.editReply({embeds: [embed]})
-    }
-    
     if(interaction.options.getSubcommand() == "submit_time"){
 
         var userID = interaction.options.getUser('user').id;
@@ -100,16 +44,11 @@ module.exports.run = async(interaction, config, maps, client) => {
             data[map] = {}
         }
         
-        var totalSeconds = (Number(totalTime[0]) * 3600) + (Number(totalTime[1]) * 60) + (Number(totalTime[2]));
-        
-        if (totalSeconds < 0) 
-        {
-            totalSeconds = 86400 + totalSeconds;
-        }
-        
+        var totalMilliseconds = (Number(totalTime[0]) * 3600) + (Number(totalTime[1]) * 60) + (Number(totalTime[2]));
+
         if(data[map][userID] != undefined)
         {
-            if(data[map][userID][0] <= totalSeconds)
+            if(data[map][userID][0] <= totalMilliseconds)
             {
                 var embed = new Discord.MessageEmbed()
                 .setTitle("No Time Recorded")
@@ -118,17 +57,17 @@ module.exports.run = async(interaction, config, maps, client) => {
             }
         }
         
-        data[map][userID] = [totalSeconds, new Date().toJSON()]
+        data[map][userID] = [totalMilliseconds, new Date().toJSON()]
         var writedata = JSON.stringify(data, null, "\t");
         await fs.writeFileSync('./speedrun_data.json', writedata);
         
         var date = new Date(null);
-        date.setSeconds(totalSeconds);
+        date.setMilliseconds(totalMilliseconds);
         var timeString = date.toISOString().slice(11, 19);
         
         var embed = new Discord.MessageEmbed()
         .setTitle("Time Recorded")
-        .setDescription(`Recorded a time of **${timeString}** (${totalSeconds} seconds) for <@${userID}> on **${map}**`);
+        .setDescription(`Recorded a time of **${timeString}** (${totalMilliseconds} seconds) for <@${userID}> on **${map}**`);
         return await interaction.editReply({embeds: [embed]})
     }
 
@@ -184,40 +123,6 @@ module.exports.info = {
     "name": "speedrun_scores",
     "description": "Allows moderators to manage scores on the boards",
     "options": [
-        {
-            "name": "submit_range",
-            "description": "Submit a users speedrun score to a map by providing the Start and Current times.",
-            "type": 1,
-            "options": [
-                {
-                    "name": "user",
-                    "description": "The user to submit for",
-                    "type": 6,
-                    "required": true
-                },
-                {
-                    "name": "map",
-                    "description": "The map they got it on",
-                    "type": 3,
-                    "autocomplete": true,
-                    "required": true
-                },
-                {
-                    "name": "start_time",
-                    "description": "Time start (in 24 hr format HH-mm-ss)",
-                    "type": 3,
-                    "autocomplete": true,
-                    "required": true
-                },
-                {
-                    "name": "end_time",
-                    "description": "Time finish (in 24 hr format HH-mm-ss)",
-                    "type": 3,
-                    "autocomplete": true,
-                    "required": true
-                },
-            ]
-        },
         {
             "name": "submit_time",
             "description": "Submit a users speedrun score to a map by providing the total time.",
