5ed426b8acd8e8e303224a0b5705cd310054af4b
Daedalline <daedalline@gmail.com>
2024-06-28 18:35:30 -0400

    Create champion-stats.js

---
+++ commands/champion-stats.js
@@ -0,0 +1,34 @@
+/*
+ * ====================NOTE====================
+ *    This code was created by Daedalline,
+ *   please don't claim this as your own work
+ *        https://github.com/Daedalline
+ * ============================================
+ */
+
+const Discord = require("discord.js");
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const fs = require("fs");
+
+module.exports.run = async(interaction, config, maps, client) => {
+    
+    var userID = interaction.options.getUser('user').id
+    var guild = await client.guilds.cache.find(guild => guild.id == interaction.guild.id)
+    var member = await guild.members.cache.find(user => user.id == interaction.member.id)
+    
+    if(!(interaction.channel.id == config.CourseDailyChannelID || interaction.channel.id == config.CourseCommunityChannelID) && !member.permissions.has("MANAGE_MESSAGES")){
+        interaction.reply({ephemeral: true, content: "You are not allowed to do that in this channel"})
+        return
+    }
+    await interaction.deferReply();
+    
+    let rawdata = await fs.readFileSync('daily_challenge_data.json');
+    let challenge_data = await JSON.parse(rawdata);
+    
+    console.log(challenge_data);
+    
+}
+module.exports.info = {
+    "name": "champion-stats",
+    "description": "List all users with First Place medals"
+};
