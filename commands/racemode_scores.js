7b76d00c5e3a93c3284be381bbed045dccae5a4a
Froman5000 <133929832+Froman5000@users.noreply.github.com>
2026-03-07 12:30:17 -0500

    Combining regular and weekly race mode score logic (#78)
    * Update racemode_scores.js
    
    * Update racemode_scores.js
    
    * Update racemode_scores.js
    
    * Update racemode_scores.js
    
    * Update racemode_scores.js
    
    * Update racemode_scores.js
    
    * Update racemode_scores.js
    
    * Update racemode_scores.js
    
    * Update racemode_scores.js
    
    * Update racemode_scores.js
    
    * Update racemode_scores.js
    
    * Update racemode_scores.js
    
    * Update racemode_scores.js
    
    * Update racemode_scores.js
    
    * Update racemode_scores.js

--- commands/racemode_scores.js
+++ commands/racemode_scores.js
@@ -2,7 +2,7 @@
  * ====================NOTE====================
  *    This code was created by Daedalline,
  *   please don't claim this as your own work
- *        https://github.com/Daedalline
+ *        https://github.com
  * ============================================
  */
 
@@ -24,55 +24,77 @@ module.exports.run = async(interaction, config, maps, client) => {
     let rawdata = await fs.readFileSync('racemode_data.json');
     let data = await JSON.parse(rawdata); 
 
+    // Read the maps.json to get the current Leaderboards list
+    let mapsFileRaw = await fs.readFileSync('maps.json');
+    let mapsFileData = await JSON.parse(mapsFileRaw);
+    let leaderboardList = mapsFileData.Leaderboards || [];
+
     if(interaction.options.getSubcommand() == "submit_time"){
 
         var userID = interaction.options.getUser('user').id;
         var map = interaction.options.getString('map');
         var time = interaction.options.getString('total_time');
-        
+    
+        const isAlreadyWeekly = map.startsWith("Weekly: ") && map.endsWith(" (Race Mode)");
+        const weeklyMapName = isAlreadyWeekly ? null : `Weekly: ${map} (Race Mode)`;
+    
         let timePattern = /\d{2}:\d{2}\.\d{2}/;
         if (!timePattern.test(time)){
             await interaction.reply({ephemeral: true, content: "Invalid time format. Time must be input as mm:ss.SS."})
             return;
         }
-    
-        var totalTime = interaction.options.getString('total_time').split(/:|\./);
 
+        var totalTime = interaction.options.getString('total_time').split(/:|\./);
         await interaction.deferReply()
-        
-        if(!data[map]){
-            data[map] = {}
-        }
-        
+    
         var totalMilliseconds = (Number(totalTime[0]) * 60000) + (Number(totalTime[1]) * 1000) + (Number(totalTime[2]) * 10);
 
-        if(data[map][userID] != undefined)
-        {
-            if(data[map][userID][0] <= totalMilliseconds)
-            {
-                var embed = new Discord.MessageEmbed()
-                .setTitle("No Time Recorded")
-                .setDescription(`This time is greater than the existing time of ` + data[map][userID][0] + " milliseconds.");
-                return await interaction.editReply({embeds: [embed]})
+        const updateMapData = (mapName) => {
+            if(!data[mapName]) data[mapName] = {};
+        
+            if(data[mapName][userID] == undefined || totalMilliseconds < data[mapName][userID][0]) {
+                data[mapName][userID] = [totalMilliseconds, new Date().toJSON()]
+                return true; 
+            }
+            return false;
+        }
+
+        let updatedBoards = [];
+
+        // Update the base map selected in the command
+        if (updateMapData(map)) {
+            updatedBoards.push(`• ${map}`);
+        }
+    
+        // Only update the Weekly board if the formatted name exists in the Leaderboards array in maps.json
+        if (weeklyMapName !== null && leaderboardList.includes(weeklyMapName)) {
+            if (updateMapData(weeklyMapName)) {
+                updatedBoards.push(`• ${weeklyMapName}`);
             }
         }
+
+        if(updatedBoards.length === 0) {
+            var embed = new Discord.MessageEmbed()
+                .setTitle("No Time Recorded")
+                .setDescription(`The submitted time is either greater than or equal to the existing time that this user is already on the Leaderboard with.`);
+            return await interaction.editReply({embeds: [embed]})
+        }
         
-        data[map][userID] = [totalMilliseconds, new Date().toJSON()]
         var writedata = JSON.stringify(data, null, "\t");
         await fs.writeFileSync('./racemode_data.json', writedata);
-        
+    
         var date = new Date(null);
         date.setMilliseconds(totalMilliseconds);
         var timeString = date.toISOString().slice(14, 22);
-        
+    
         var embed = new Discord.MessageEmbed()
-        .setTitle("Time Recorded")
-        .setDescription(`Recorded a time of **${timeString}** (${totalMilliseconds} milliseconds) for <@${userID}> on **${map}**`);
+            .setTitle("Time Recorded")
+            .setDescription(`Recorded **${timeString}** for <@${userID}>!\n\n**Boards Updated:**\n${updatedBoards.join("\n")}`);
+        
         return await interaction.editReply({embeds: [embed]})
     }
 
     if(interaction.options.getSubcommand() == "remove"){
-
         var userID = interaction.options.getUser('user').id
         var map = interaction.options.getString('map')
 
@@ -106,7 +128,7 @@ module.exports.autocomplete = async (interaction, Maps) => {
     switch(value.name){
         case 'map': {
             Maps.Leaderboards.forEach(map => {
-                if((map.toLowerCase().includes(value.value.toLowerCase()) || value == "") && !(map.startsWith("Weekly") && !map.endsWith("(Race Mode)"))){
+                if((map.toLowerCase().includes(value.value.toLowerCase()) || value.value == "") && !(map.startsWith("Weekly") && !map.endsWith("(Race Mode)"))){
                     res.push({
                         name: map,
                         value: map
@@ -145,7 +167,6 @@ module.exports.info = {
                     "name": "total_time",
                     "description": "Total Time (mm:ss.SS)",
                     "type": 3,
-                    "autocomplete": true,
                     "required": true
                 },
             ]
